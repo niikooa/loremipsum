febfb50f8fdf081a33dc31dd554cf4c0ead77472
niikooa <nico_ambrosino@hotmail.com>
2023-04-20 18:35:29 -0300

    Agregado archivo lipsum.sh

--- lipsum.sh
+++ lipsum.sh
@@ -2,6 +2,11 @@
 
 for i in {1..5}
 do
-  curl -L "https://lipsum.com/feed/json" | jq '.feed.entry.content.$t' | sed 's/<[^>]*>//g' | head -c 1000 > "lipsum_$i.txt"
+    #obtiene una cantidad aleatoria de número de lineas entre 1 y 20
+    lineas=$(shuf -i 1-20 -n 1)
+    #llama con curl a lipsum.com y le pasa como parámetro la cantidad de líneas
+    lorem=$(curl -s https://www.lipsum.com/feed/xml\?amount\=$lineas | sed -n '/<lipsum>/,/<\/lipsum>/p' | sed -e 's/<lipsum>//g' -e 's/<\/lipsum>//g'
+)   
+    #crea un archivo con el contenido de la variable lorem
+    echo "$lorem" >> loremipsum-$i.txt
 done
-
